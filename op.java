1b8e8f5b76dac06aad8be73a3a3a1d89ce1cf14d
alye <alizishaan.khatri@gmail.com>
2015-04-04 00:24:26 +0530

    Update op.java

--- op.java
+++ op.java
@@ -1,21 +1,20 @@
-       class op implements ActionListener
-        {
-            Calc c;
-            Button b;
-            op(Calc x,Button y)
-            {
-                c=x;
-                b=y;
-            }
+public class op implements ActionListener
+ {
+     Calc c;
+     Button b;
+     op(Calc x,Button y)
+     {
+         c=x;
+         b=y;
+     }
+     public void actionPerformed(ActionEvent e)
+     {
+         if(e.getSource()==b)
+         {
+             c.s=b.getLabel();
+             c.d=Double.parseDouble(c.t.getText());
+             c.t.setText(""+0);
 
-            public void actionPerformed(ActionEvent e)
-            {
-                if(e.getSource()==b)
-                {
-                    c.s=b.getLabel();
-                    c.d=Double.parseDouble(c.t.getText());
-                    c.t.setText(""+0);
-
-                }
-            }
-        }
+         }
+     }
+ }

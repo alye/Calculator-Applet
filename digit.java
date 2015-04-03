61b1783a824a01b066b3843c9c9ae6eb7790293f
alye <alizishaan.khatri@gmail.com>
2015-04-03 23:53:26 +0530

    Create digit.java

---
+++ digit.java
@@ -0,0 +1,19 @@
+class digit implements ActionListener
+        {
+            Button b;
+            Calc c;
+            digit(Calc x,Button i)
+            {
+                b=i;
+                c=x;
+            }
+
+            public void actionPerformed(ActionEvent e)
+            {
+                if(e.getSource()==b)
+                    c.t.setText(c.t.getText() +b.getLabel());
+            }
+
+        }
+
+ 

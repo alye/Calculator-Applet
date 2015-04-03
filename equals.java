b84bdf264d6d790a60e95e8e9d8f1e62cf6d78c2
alye <alizishaan.khatri@gmail.com>
2015-04-03 23:54:49 +0530

    Create equals.java

---
+++ equals.java
@@ -0,0 +1,33 @@
+  class equals implements ActionListener
+        {
+            Calc c;
+            Button b;
+
+            equals(Calc x)
+            {
+                c=x;
+                b=c.sign[4];
+            }
+
+            public void actionPerformed(ActionEvent e)
+            {
+                if(e.getSource()==b)
+                {
+                   double no2=Double.parseDouble(c.t.getText());
+
+                   if(c.s.equalsIgnoreCase("+"))
+                       c.d+=no2;
+                   else if(c.s.equalsIgnoreCase("-"))
+                       c.d-=no2;
+                   else if(c.s.equalsIgnoreCase("*"))
+                       c.d*=no2;
+                   else if(c.s.equalsIgnoreCase("/"))
+                       c.d/=no2;
+
+                   c.t.setText(""+c.d);
+
+
+                }
+
+            }
+        }

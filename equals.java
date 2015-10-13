b98639b141c2cee91a9f339d3bacde07226f3c47
Alizishaan Khatri <alizishaan.khatri@gmail.com>
2015-10-13 01:41:36 -0400

    Update equals.java

--- equals.java
+++ equals.java
@@ -1,35 +1,42 @@
+ /**
+ * Class to process the pressing of '= sign
+ */
  package com.apps.calculator;
  
  public class equals implements ActionListener
+ {
+   private Calc c;            //Local Instance of Calc created to store reference to the class whose '=' sign is pressed
+   private Button b;          //Local Instance of Button created to store reference to the respective '=' Button
+   /**
+     * Constructor to load the private variables of equals class
+     * @param Calc A reference to the Calc class which contains the equals Button
+     * @return Void/No return Value
+     * @exception Any Exception
+     */
+     equals(Calc x)
+     {
+       c=x;
+       b=c.sign[4];
+     }
+     public void actionPerformed(ActionEvent e)
+     {
+        if(e.getSource()==b)
         {
-            Calc c;
-            Button b;
-
-            equals(Calc x)
-            {
-                c=x;
-                b=c.sign[4];
-            }
-
-            public void actionPerformed(ActionEvent e)
-            {
-                if(e.getSource()==b)
-                {
-                   double no2=Double.parseDouble(c.t.getText());
-
-                   if(c.s.equalsIgnoreCase("+"))
-                       c.d+=no2;
-                   else if(c.s.equalsIgnoreCase("-"))
-                       c.d-=no2;
-                   else if(c.s.equalsIgnoreCase("*"))
-                       c.d*=no2;
-                   else if(c.s.equalsIgnoreCase("/"))
-                       c.d/=no2;
-
-                   c.t.setText(""+c.d);
-
-
-                }
-
-            }
-        }
+          private double no2=Double.parseDouble(c.t.getText());       //Temp Variable to store operand 2
+          /**
+           * Loading sign information and performing the desired operation between operand one and operand 2
+           */
+           if(c.s.equalsIgnoreCase("+"))
+              c.d+=no2;
+           else if(c.s.equalsIgnoreCase("-"))
+              c.d-=no2;
+           else if(c.s.equalsIgnoreCase("*"))
+              c.d*=no2;
+           else if(c.s.equalsIgnoreCase("/"))
+              c.d/=no2;
+           /** Update the main layout's display
+            */
+            c.t.setText(""+c.d);
+          }
+      }
+ }

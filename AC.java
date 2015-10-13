8176587def92ef8190d0398a8b2a4f17a705903f
Alizishaan Khatri <alizishaan.khatri@gmail.com>
2015-10-13 00:48:51 -0400

    Update AC.java

--- AC.java
+++ AC.java
@@ -1,19 +1,34 @@
-        public class AC implements ActionListener
+/**
+ * Class to perform the 'AC' operation.
+ */
+ public class AC implements ActionListener
+{
+    private Calc c;                     //Local Instance of Calc created to store reference to the calling class
+    private Button b;                   //Local Instance of Button created to store reference to the Ac
+    /**
+     * Constructor to pass instance of the calling Calc class to the AC class
+     */
+    AC(Calc x)                          
+    {
+        c=x;
+        b=c.sign[5];
+    }
+    
+    /** 
+     * Implementing actionPerformed(ActionEvent) method of ActionListener interface.
+     * Specifies steps to be taken when AC button is clicked
+     */
+     public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource()==b)
         {
-            Calc c;
-            Button b;
-            AC(Calc x)
-            {
-                c=x;
-                b=c.sign[5];
-            }
-            public void actionPerformed(ActionEvent e)
-            {
-                if(e.getSource()==b)
-                {
-                    c.d=0;
-                    c.s="";
-                    c.t.setText("0");
-                }
-            }
+                /** 
+                 * Clear memory of parent class
+                 */
+                c.d=0;
+                c.s="";
+                c.t.setText("0");
         }
+            
+    }
+}
